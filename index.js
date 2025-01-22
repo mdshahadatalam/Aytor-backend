ce5de41eaa7c0bc3274a807d65c4c63268b82b65
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-01-22 11:13:55 -0800

    all

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const mongoose = require('mongoose');
+const nodemailer = require("nodemailer");
 
 
 
@@ -12,8 +13,34 @@ mongoose.connect(`mongodb+srv://${process.env.REACT_APP_USERNAME}:${process.env.
 
 app.use(cors())
 app.use(express.json())
-app.get('/', function (req, res) {
+
+
+app.post('/sendEmail', async function (req, res) {
     res.send('Hello World')
+    console.log(req.body);
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      secure: false, // true for port 465, false for other ports
+      auth: {
+        user: process.env.EMAIL_USER, // Load from .env
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+      to: "bar@example.com, baz@example.com", // list of receivers
+      subject: req.body.message, // Subject line
+      html: ` <b>Name:</b>${req.body.name}
+               <b>Email:</b>${req.body.email}
+               <b>phone:</b>${req.body.phone}
+               <b>Title:</b>${req.body.title}
+               <b>Message:</b>${req.body.message}
+      `, // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    
   })
   
   app.listen(3000)
@@ -21,4 +48,4 @@ app.get('/', function (req, res) {
 
   // mdshahadat
 
-  // mdshahadat101108
+  // mdshahadat101108    
